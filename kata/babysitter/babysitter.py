92d933e922af27ad4da00129437d85b1180a9d88
Max Muir <weezbo@yahoo.com>
2018-10-03 16:41:45 -0400

    Enforcing end times

--- kata/babysitter/babysitter.py
+++ kata/babysitter/babysitter.py
@@ -2,7 +2,10 @@ def babysitter_pay_calc(start_time, end_time, bed_time = False):
     minimum_start_time = 17
     maximum_end_time = 4
     if start_time in range(maximum_end_time, minimum_start_time):
-        return "Start or end time is out of bounds"
+        return "Start time is out of bounds"
+    elif end_time in range(maximum_end_time + 1, minimum_start_time + 1):
+        return "End time is out of bounds"
     else:
         return "Acceptable times"
 
+babysitter_pay_calc(17, 4)
