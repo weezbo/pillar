070db9a510acb21507223f8a2fdfc957cf9c5bbb
Max Muir <weezbo@yahoo.com>
2018-10-03 14:59:06 -0400

    Making minimum hours 1

--- kata/babysitter/babysitter.py
+++ kata/babysitter/babysitter.py
@@ -5,5 +5,8 @@ def calculate_total_for_period(hours, rate):
 def calculate_hours_for_period(start_time, end_time):
     start_hour = int(start_time[0:2])
     end_hour = int(end_time[0:2])
-    return end_hour - start_hour
+    total_hours = end_hour - start_hour
+    if total_hours < 1:
+        total_hours = 1
+    return total_hours
 
--- kata/babysitter/babysitter_test.py
+++ kata/babysitter/babysitter_test.py
@@ -14,3 +14,6 @@ class BabysitterTests(unittest.TestCase):
     def test_calculate_hours_for_after_midnight(self):
         self.assertEqual(calculate_hours_for_period('0000', '0400'), 4)
         self.assertEqual(calculate_hours_for_period('0200', '0400'), 2)
+        #only wanting whole hours, rounded up
+        self.assertEqual(calculate_hours_for_period('0200', '0230'), 1)
+
