e647ed21eaf1189ee32da7d7e5a4119403dad972
Max Muir <weezbo@yahoo.com>
2018-10-03 15:55:35 -0400

    Starting over. Enforcing minimum start time

--- kata/babysitter/babysitter.py
+++ kata/babysitter/babysitter.py
@@ -1,20 +1,5 @@
-
-def calculate_total_for_period(hours, rate):
-    return hours*rate;
-
-def calculate_hours_for_period(start_time, end_time):
-    if start_time < 4:
-        start_time += 24
-    if end_time < 17:
-        end_time += 24
-    return end_time - start_time
-
 def babysitter_pay_calc(start_time, end_time, bed_time = False):
-    pre_bed_rate = 12
-    post_midnight_rate = 16
-    if(end_time < 4 and end_time != 0):
-        rate = post_midnight_rate
-    else:
-        rate = pre_bed_rate
-    hours = calculate_hours_for_period(start_time, end_time)
-    return calculate_total_for_period(hours, rate)
+    minimum_start_time = 17
+    if start_time < minimum_start_time:
+        return "Start or end time is out of bounds"
+
--- kata/babysitter/babysitter_test.py
+++ kata/babysitter/babysitter_test.py
@@ -1,27 +1,5 @@
 import unittest
 from babysitter import *
 class BabysitterTests(unittest.TestCase):
-    def test_calculate_standard_total(self):
-        self.assertEqual(calculate_total_for_period(4, 12), 48)
-        self.assertEqual(calculate_total_for_period(3, 12), 36)
-    def test_calculate_post_bedtime_total(self):
-        self.assertEqual(calculate_total_for_period(2, 8), 16)
-        self.assertEqual(calculate_total_for_period(0, 8), 0)
-        self.assertEqual(calculate_total_for_period(1, 8), 8)
-    def test_calculate_post_midnight_total(self):
-        self.assertEqual(calculate_total_for_period(3, 16), 48)
-        self.assertEqual(calculate_total_for_period(2, 16), 32)
-    def test_calculate_hours_for_after_midnight(self):
-        self.assertEqual(calculate_hours_for_period(0, 4), 4)
-        self.assertEqual(calculate_hours_for_period(2, 4), 2)
-    def test_calculate_hours_before_midnight(self):
-        self.assertEqual(calculate_hours_for_period(17, 23), 6)
-        self.assertEqual(calculate_hours_for_period(18, 23), 5)
-    def test_calculate_hours_for_full_shift(self):
-        self.assertEqual(calculate_hours_for_period(18, 3), 9)
-        self.assertEqual(calculate_hours_for_period(23, 2), 3)
-    def test_calculate_pre_midnight_total_no_bedtime(self):
-        self.assertEqual(babysitter_pay_calc(20, 0), 48)
-    def test_calculate_post_midnight_total_no_bedtime(self):
-        self.assertEqual(babysitter_pay_calc(0, 2), 32)
-
+    def test_enforce_minimum_time(self):
+        self.assertEqual(babysitter_pay_calc(12, 4, 14), "Start or end time is out of bounds")
