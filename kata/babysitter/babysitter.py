0838043e419f1f3d5a50617e71c21c1c514136b3
Max Muir <weezbo@yahoo.com>
2018-10-03 16:54:04 -0400

    Moved real time check to a function

--- kata/babysitter/babysitter.py
+++ kata/babysitter/babysitter.py
@@ -1,9 +1,15 @@
 def babysitter_pay_calc(start_time, end_time, bed_time = False):
     minimum_start_time = 17
     maximum_end_time = 4
-    if start_time in range(maximum_end_time, minimum_start_time) or start_time > 24 or start_time < 0:
+    if start_time in range(maximum_end_time, minimum_start_time) or not real_time(start_time):
         return "Start time is out of bounds"
-    elif end_time in range(maximum_end_time + 1, minimum_start_time + 1) or end_time > 24 or end_time < 0:
+    elif end_time in range(maximum_end_time + 1, minimum_start_time + 1) or not real_time(end_time):
         return "End time is out of bounds"
     else:
         return "Acceptable times"
+
+def real_time(candidate):
+    if candidate < 0 or candidate > 24:
+        return False
+    else:
+        return True
