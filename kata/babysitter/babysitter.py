da0cf50e3579af2e07a416fc9abfa2ab0a5de987
Max Muir <weezbo@yahoo.com>
2018-10-03 16:08:12 -0400

    Enforcing minimum start time over more cases

--- kata/babysitter/babysitter.py
+++ kata/babysitter/babysitter.py
@@ -1,5 +1,8 @@
 def babysitter_pay_calc(start_time, end_time, bed_time = False):
     minimum_start_time = 17
-    if start_time < minimum_start_time:
+    maximum_end_time = 4
+    if start_time in range(maximum_end_time, minimum_start_time):
         return "Start or end time is out of bounds"
+    else:
+        return "Acceptable times"
 
--- kata/babysitter/babysitter_test.py
+++ kata/babysitter/babysitter_test.py
@@ -2,4 +2,9 @@ import unittest
 from babysitter import *
 class BabysitterTests(unittest.TestCase):
     def test_enforce_minimum_time(self):
-        self.assertEqual(babysitter_pay_calc(12, 4, 14), "Start or end time is out of bounds")
+        self.assertEqual(babysitter_pay_calc(12, 4), "Start or end time is out of bounds")
+        #whoops, we can start after midnight
+        self.assertEqual(babysitter_pay_calc(3, 4), "Acceptable times")
+        #also should allow starting at 17 but not starting at 4
+        self.assertEqual(babysitter_pay_calc(17, 4), "Acceptable times")
+        self.assertEqual(babysitter_pay_calc(4, 4), "Start or end time is out of bounds")
