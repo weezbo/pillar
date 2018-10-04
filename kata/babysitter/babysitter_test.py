3a967274bf257cc868db306576e3e2bb15244e8d
Max Muir <weezbo@yahoo.com>
2018-10-04 14:29:23 -0400

    Refactor of test module for PEP 8

--- kata/babysitter/babysitter_test.py
+++ kata/babysitter/babysitter_test.py
@@ -1,9 +1,9 @@
 import unittest
 from babysitter import Babysitter
 Babysitter = Babysitter()
-pre_bed = 12
-post_bed = 8
-post_mid = 16
+PRE_BED = 12
+POST_BED = 8
+POST_MID = 16
 class BabysitterTests(unittest.TestCase):
     def test_enforce_minimum_time(self):
         self.assertEqual(Babysitter.calculate_pay(12, 4), "Start time is out of bounds")
@@ -27,22 +27,22 @@ class BabysitterTests(unittest.TestCase):
         self.assertEqual(Babysitter.calculate_pay(3, 4, 25), "Bed time is out of bounds")
         self.assertEqual(Babysitter.calculate_pay(3, 4, -1), "Bed time is out of bounds")
     def test_pre_midnight_rate_no_bedtime(self):
-        self.assertEqual(Babysitter.calculate_pay(17, 20, False), 3 * pre_bed)
+        self.assertEqual(Babysitter.calculate_pay(17, 20, False), 3 * PRE_BED)
     def test_post_midnight_rate(self):
-        self.assertEqual(Babysitter.calculate_pay(2, 4, 3), 2 * post_mid)
+        self.assertEqual(Babysitter.calculate_pay(2, 4, 3), 2 * POST_MID)
     def test_cross_midnight_rate_no_bedtime(self):
-        self.assertEqual(Babysitter.calculate_pay(23, 2, False), (1 * pre_bed) + (2 * post_mid))
+        self.assertEqual(Babysitter.calculate_pay(23, 2, False), (1 * PRE_BED) + (2 * POST_MID))
     def test_pre_midnight_rate_with_bedtime(self):
-        self.assertEqual(Babysitter.calculate_pay(17, 23, 19), (2 * pre_bed) + (4 * post_bed))
+        self.assertEqual(Babysitter.calculate_pay(17, 23, 19), (2 * PRE_BED) + (4 * POST_BED))
     def test_cross_midnight_rate_with_bedtime(self):
-        self.assertEqual(Babysitter.calculate_pay(17, 4, 19), (2 * pre_bed) + (5 * post_bed) + (4 * post_mid))
+        self.assertEqual(Babysitter.calculate_pay(17, 4, 19), (2 * PRE_BED) + (5 * POST_BED) + (4 * POST_MID))
     def test_ends_at_midnight_with_bedtime(self):
-        self.assertEqual(Babysitter.calculate_pay(17, 0, 19), (2 * pre_bed) + (5 * post_bed))
-        self.assertEqual(Babysitter.calculate_pay(17, 24, 19), (2 * pre_bed) + (5 * post_bed))
+        self.assertEqual(Babysitter.calculate_pay(17, 0, 19), (2 * PRE_BED) + (5 * POST_BED))
+        self.assertEqual(Babysitter.calculate_pay(17, 24, 19), (2 * PRE_BED) + (5 * POST_BED))
     def test_cross_midnight_midnight_bedtime(self):
-        self.assertEqual(Babysitter.calculate_pay(23, 2, 0), (1 * pre_bed) + (2 * post_mid))
-        self.assertEqual(Babysitter.calculate_pay(23, 4, 24), (1 * pre_bed) + (4 * post_mid))
+        self.assertEqual(Babysitter.calculate_pay(23, 2, 0), (1 * PRE_BED) + (2 * POST_MID))
+        self.assertEqual(Babysitter.calculate_pay(23, 4, 24), (1 * PRE_BED) + (4 * POST_MID))
     def test_equal_start_and_end_time(self):
         self.assertEqual(Babysitter.calculate_pay(18, 18, 19), "End time must be later than start time")
     def test_early_bedtime(self):
-        self.assertEqual(Babysitter.calculate_pay(17, 4, 14), (7 * post_bed) + (4 * post_mid))
+        self.assertEqual(Babysitter.calculate_pay(17, 4, 14), (7 * POST_BED) + (4 * POST_MID))
