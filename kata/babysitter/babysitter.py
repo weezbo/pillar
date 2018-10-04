0893b8a2d4092b77fa693e68e07096f8e0e36269
Max Muir <weezbo@yahoo.com>
2018-10-04 12:57:46 -0400

    Refactor of function names

--- kata/babysitter/babysitter.py
+++ kata/babysitter/babysitter.py
@@ -5,44 +5,47 @@ class Babysitter:
     minimum_start_time = 17
     maximum_end_time = 4
 
-    def babysitter_pay_calc(self, start_time, end_time, bed_time = False):
+    def calculatePay(self, start_time, end_time, bed_time = False):
         total_pay = 0
         try:
-            times = self.time_enforcer(start_time, end_time, bed_time)
+            times = self.timeVerifier(start_time, end_time, bed_time)
         except Exception as e:
             return str(e)
-        for i in range(times[0], times[1]):
+        converted_start_time = times[0]
+        converted_end_time = times[1]
+        converted_bed_time = times[2]
+        for i in range(converted_start_time, converted_end_time):
             if i > 23:
                 total_pay += self.post_midnight_rate
-            elif times[2] and i >= times[2]:
+            elif converted_bed_time and i >= converted_bed_time:
                 total_pay += self.post_bedtime_rate
             else:
                 total_pay += self.pre_bedtime_rate
         return total_pay
 
-    def real_time(self, candidate):
+    def verifyRealTime(self, candidate):
         if candidate < 0 or candidate > 24:
             return False
         else:
             return True
 
-    def midnight_converter(self, hour):
-        if hour is False or hour > 5:
+    def midnightConverter(self, hour):
+        if hour is False or hour > 4:
             return hour
         else:
             return hour + 24
 
 
-    def time_enforcer(self, start_time, end_time, bed_time = False):
-        if start_time in range(self.maximum_end_time, self.minimum_start_time) or not self.real_time(start_time):
+    def timeVerifier(self, start_time, end_time, bed_time = False):
+        if start_time in range(self.maximum_end_time, self.minimum_start_time) or not self.verifyRealTime(start_time):
             raise Exception("Start time is out of bounds")
-        if end_time in range(self.maximum_end_time + 1, self.minimum_start_time + 1) or not self.real_time(end_time):
+        if end_time in range(self.maximum_end_time + 1, self.minimum_start_time + 1) or not self.verifyRealTime(end_time):
             raise Exception("End time is out of bounds")
-        if bed_time and not self.real_time(bed_time):
+        if bed_time and not self.verifyRealTime(bed_time):
             raise Exception("Bed time is out of bounds")
-        start_time = self.midnight_converter(start_time)
-        end_time = self.midnight_converter(end_time)
-        bed_time = self.midnight_converter(bed_time)
+        start_time = self.midnightConverter(start_time)
+        end_time = self.midnightConverter(end_time)
+        bed_time = self.midnightConverter(bed_time)
         if end_time <= start_time:
             raise Exception("End time must be later than start time")
         return start_time, end_time, bed_time
--- kata/babysitter/babysitter_test.py
+++ kata/babysitter/babysitter_test.py
@@ -6,43 +6,43 @@ post_bed = 8
 post_mid = 16
 class BabysitterTests(unittest.TestCase):
     def test_enforce_minimum_time(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(12, 4), "Start time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(12, 4), "Start time is out of bounds")
         #whoops, we can start after midnight
-        self.assertGreater(Babysitter.babysitter_pay_calc(3, 4), 0)
+        self.assertGreater(Babysitter.calculatePay(3, 4), 0)
         #also should allow starting at 17 but not starting at 4
-        self.assertGreater(Babysitter.babysitter_pay_calc(17, 4), 0)
-        self.assertEqual(Babysitter.babysitter_pay_calc(4, 4), "Start time is out of bounds")
+        self.assertGreater(Babysitter.calculatePay(17, 4), 0)
+        self.assertEqual(Babysitter.calculatePay(4, 4), "Start time is out of bounds")
     def test_enforce_maximum_end_time(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 5), "End time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(17, 5), "End time is out of bounds")
     def test_enforce_clock_times(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(25, 4), "Start time is out of bounds")
-        self.assertEqual(Babysitter.babysitter_pay_calc(-1, 4), "Start time is out of bounds")
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 25), "End time is out of bounds")
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, -1), "End time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(25, 4), "Start time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(-1, 4), "Start time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(17, 25), "End time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(17, -1), "End time is out of bounds")
     def test_enforce_time_direction(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(19, 18), "End time must be later than start time")
+        self.assertEqual(Babysitter.calculatePay(19, 18), "End time must be later than start time")
         #need to check after midnight, too
-        self.assertEqual(Babysitter.babysitter_pay_calc(3, 2), "End time must be later than start time")
+        self.assertEqual(Babysitter.calculatePay(3, 2), "End time must be later than start time")
     def test_bed_time_is_real(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(3, 4, 25), "Bed time is out of bounds")
-        self.assertEqual(Babysitter.babysitter_pay_calc(3, 4, -1), "Bed time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(3, 4, 25), "Bed time is out of bounds")
+        self.assertEqual(Babysitter.calculatePay(3, 4, -1), "Bed time is out of bounds")
     def test_pre_midnight_rate_no_bedtime(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 20, False), 3 * pre_bed)
+        self.assertEqual(Babysitter.calculatePay(17, 20, False), 3 * pre_bed)
     def test_post_midnight_rate(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(2, 4, 3), 2 * post_mid)
+        self.assertEqual(Babysitter.calculatePay(2, 4, 3), 2 * post_mid)
     def test_cross_midnight_rate_no_bedtime(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(23, 2, False), (1 * pre_bed) + (2 * post_mid))
+        self.assertEqual(Babysitter.calculatePay(23, 2, False), (1 * pre_bed) + (2 * post_mid))
     def test_pre_midnight_rate_with_bedtime(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 23, 19), (2 * pre_bed) + (4 * post_bed))
+        self.assertEqual(Babysitter.calculatePay(17, 23, 19), (2 * pre_bed) + (4 * post_bed))
     def test_cross_midnight_rate_with_bedtime(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 4, 19), (2 * pre_bed) + (5 * post_bed) + (4 * post_mid))
+        self.assertEqual(Babysitter.calculatePay(17, 4, 19), (2 * pre_bed) + (5 * post_bed) + (4 * post_mid))
     def test_ends_at_midnight_with_bedtime(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 0, 19), (2 * pre_bed) + (5 * post_bed))
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 24, 19), (2 * pre_bed) + (5 * post_bed))
+        self.assertEqual(Babysitter.calculatePay(17, 0, 19), (2 * pre_bed) + (5 * post_bed))
+        self.assertEqual(Babysitter.calculatePay(17, 24, 19), (2 * pre_bed) + (5 * post_bed))
     def test_cross_midnight_midnight_bedtime(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(23, 2, 0), (1 * pre_bed) + (2 * post_mid))
-        self.assertEqual(Babysitter.babysitter_pay_calc(23, 4, 24), (1 * pre_bed) + (4 * post_mid))
+        self.assertEqual(Babysitter.calculatePay(23, 2, 0), (1 * pre_bed) + (2 * post_mid))
+        self.assertEqual(Babysitter.calculatePay(23, 4, 24), (1 * pre_bed) + (4 * post_mid))
     def test_equal_start_and_end_time(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(18,18,19), "End time must be later than start time")
+        self.assertEqual(Babysitter.calculatePay(18, 18, 19), "End time must be later than start time")
     def test_early_bedtime(self):
-        self.assertEqual(Babysitter.babysitter_pay_calc(17, 4, 14), (7 * post_bed) + (4 * post_mid ))
+        self.assertEqual(Babysitter.calculatePay(17, 4, 14), (7 * post_bed) + (4 * post_mid))
